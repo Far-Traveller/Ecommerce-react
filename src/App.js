b60f6da1abeef82b35b50e067e9ec796acb05723
Far-traveller <carriere.aurore@orange.fr>
2022-06-14 16:51:49 +0200

    router created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+
+import Navbar from './Components/Navbar/Navbar';
+import FloatingCart from './Components/FloatingCart/FloatingCart';
+
+import Home from './Pages/Home/Home';
+import Contact from './Pages/Contact/Contact';
+import ShoppingCart from './Pages/ShopppingCart/ShoppingCart';
+import Products from './Pages/Products/Products';
+import ProductShowcase from './Pages/ProductShowcase/ProductShowcase';
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/panier' element={<ShoppingCart />} />
+        <Route path='/produits' element={<Products />} />
+        <Route path='/produits/:id' element={<ProductShowcase />} />
+      </Routes>
+    </BrowserRouter>
 
-    </>
   );
 }
 
 export default App;
+
